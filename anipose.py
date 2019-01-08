7be971e4bbaaa1a763f0199e8e9cb742c11c0758
Pierre Karashchuk <krchtchk@gmail.com>
2019-01-08 13:35:16 -0800

    update commands, all the basic ones are here!

--- anipose.py
+++ anipose.py
@@ -10,7 +10,7 @@ import click
 ## possible commands
 # anipose calibrate # run calibration of intrinsics and extrinsics
 # anipose analyze # analyze the poses for each video
-# anipose label # create videos for each pose 
+# anipose label # create videos for each pose
 # anipose run-data # run only the data portion (no viz)
 # anipose run-viz # run only the visualization pipeline
 # anipose run-all # run everything (run-data then run-viz)
@@ -30,7 +30,7 @@ DEFAULT_CONFIG = {
 def load_config(fname):
     if fname is None:
         fname = 'config.toml'
-    
+
     if os.path.exists(fname):
         config = toml.load(fname)
     else:
@@ -43,7 +43,7 @@ def load_config(fname):
     for k,v in DEFAULT_CONFIG.items():
         if k not in config:
             config[k] = v
-        
+
     return config
 
 @click.group()
@@ -57,7 +57,6 @@ def cli(ctx, config):
 @pass_config
 def calibrate(config):
     click.echo('Calibrating...')
-    click.echo(config)
     calibrate_intrinsics_all(config)
     calibrate_extrinsics_all(config)
 
@@ -65,37 +64,96 @@ def calibrate(config):
 @pass_config
 def calibrate_intrinsics(config):
     click.echo('Calibrating intrinsics...')
-    click.echo(config)
     calibrate_intrinsics_all(config)
 
 @cli.command()
 @pass_config
 def calibrate_extrinsics(config):
     click.echo('Calibrating extrinsics...')
-    click.echo(config)
     calibrate_extrinsics_all(config)
-    
+
 @cli.command()
 @pass_config
 def analyze(config):
     from pose_videos import pose_videos_all
     click.echo('Analyzing videos...')
     pose_videos_all(config)
- 
+
+@cli.command()
+@pass_config
+def triangulate(config):
+    from triangulate import triangulate_all
+    click.echo('Triangulating points...')
+    triangulate_all(config)
+
+@cli.command()
+@pass_config
+def label_2d(config):
+    from label_videos import label_videos_all
+    click.echo('Labeling videos in 2D...')
+    label_videos_all(config)
+
+@cli.command()
+@pass_config
+def label_3d(config):
+    from label_videos_3d import label_videos_3d_all
+    click.echo('Labeling videos in 3D...')
+    label_videos_3d_all(config)
+
 @cli.command()
 @pass_config
 def run_data(config):
+    from pose_videos import pose_videos_all
+    from triangulate import triangulate_all
+
+    click.echo('Calibrating...')
+    calibrate_intrinsics_all(config)
+    calibrate_extrinsics_all(config)
+
+    click.echo('Analyzing videos...')
+    pose_videos_all(config)
+
+    click.echo('Triangulating points...')
+    triangulate_all(config)
+
     click.echo('Running data processing')
+
 
 @cli.command()
 @pass_config
 def run_viz(config):
-    click.echo('Running visualization')
+    from label_videos import label_videos_all
+    from label_videos_3d import label_videos_3d_all
+
+    click.echo('Labeling videos in 2D...')
+    label_videos_all(config)
+    click.echo('Labeling videos in 3D...')
+    label_videos_3d_all(config)
+
 
 @cli.command()
 @pass_config
 def run_all(config):
-    click.echo('Running all')
-    
+    from pose_videos import pose_videos_all
+    from triangulate import triangulate_all
+
+    click.echo('Calibrating...')
+    calibrate_intrinsics_all(config)
+    calibrate_extrinsics_all(config)
+
+    click.echo('Analyzing videos...')
+    pose_videos_all(config)
+
+    click.echo('Triangulating points...')
+    triangulate_all(config)
+
+    from label_videos import label_videos_all
+    from label_videos_3d import label_videos_3d_all
+
+    click.echo('Labeling videos in 2D...')
+    label_videos_all(config)
+    click.echo('Labeling videos in 3D...')
+    label_videos_3d_all(config)
+
 if __name__ == '__main__':
     cli()
