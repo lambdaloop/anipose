316cab7897d94b2d794058f4578860755bbf0889
Pierre Karashchuk <krchtchk@gmail.com>
2019-08-22 11:26:30 -0700

    bump version

--- anipose/__init__.py
+++ anipose/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 VERSION = __version__
 
 from . import common
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anipose",
-    version="0.5.2",
+    version="0.5.3",
     author="Pierre Karashchuk",
     author_email="krchtchk@gmail.com",
     description="Framework for scalable DeepLabCut based analysis including 3D tracking",
