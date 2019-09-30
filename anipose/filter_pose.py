ec5cc1d0819226d366bfa9f9b52a5b34a3063741
Pierre Karashchuk <krchtchk@gmail.com>
2019-09-29 18:38:22 -0700

    add autoencoder filter and refactor filtering

--- anipose/filter_pose.py
+++ anipose/filter_pose.py
@@ -14,6 +14,7 @@ from scipy.special import logsumexp
 from collections import Counter
 from multiprocessing import cpu_count
 from multiprocessing import Pool
+import pickle
 
 from .common import make_process_fun, natural_keys
 
@@ -139,22 +140,11 @@ def find_best_path_wrapper(args):
     pts_new, scs_new = find_best_path(pts, scs, max_offset, thres_dist)
     return jix, pts_new, scs_new
 
-def filter_pose_clusters(config, fname, outname):
-    data_orig = pd.read_hdf(fname)
-    scorer = data_orig.columns.levels[0][0]
-    data = data_orig.loc[:, scorer]
+def filter_pose_clusters(config, all_points, bodyparts):
+    n_frames, n_joints, n_possible, _ = all_points.shape
 
-    bp_index = data.columns.names.index('bodyparts')
-    coord_index = data.columns.names.index('coords')
-    bodyparts = list(data.columns.get_level_values(bp_index).unique())
-    n_possible = len(data.columns.levels[coord_index])//3
-
-    n_frames = len(data)
-    n_joints = len(bodyparts)
-    test = np.array(data).reshape(n_frames, n_joints, n_possible, 3)
-
-    points_full = test[:, :, :, :2]
-    scores_full = test[:, :, :, 2]
+    points_full = all_points[:, :, :, :2]
+    scores_full = all_points[:, :, :, 2]
 
     points_full[scores_full < config['filter']['score_threshold']] = np.nan
 
@@ -177,17 +167,7 @@ def filter_pose_clusters(config, fname, outname):
         points[:, jix] = pts_new
         scores[:, jix] = scs_new
 
-    columns = pd.MultiIndex.from_product(
-        [[scorer], bodyparts, ['x', 'y', 'likelihood']],
-        names=['scorer', 'bodyparts', 'coords'])
-
-    dout = pd.DataFrame(columns=columns, index=data.index)
-
-    dout.loc[:, (scorer, bodyparts, 'x')] = points[:, :, 0]
-    dout.loc[:, (scorer, bodyparts, 'y')] = points[:, :, 1]
-    dout.loc[:, (scorer, bodyparts, 'likelihood')] = scores
-
-    dout.to_hdf(outname, 'df_with_missing', format='table', mode='w')
+    return points, scores
 
 def viterbi_path(points, scores, n_back=3, thres_dist=30):
     n_frames = points.shape[0]
@@ -270,7 +250,7 @@ def viterbi_path_wrapper(args):
     return jix, pts_new, scs_new
 
 
-def filter_pose_viterbi(config, fname, outname):
+def load_pose_2d(fname):
     data_orig = pd.read_hdf(fname)
     scorer = data_orig.columns.levels[0][0]
     data = data_orig.loc[:, scorer]
@@ -284,8 +264,19 @@ def filter_pose_viterbi(config, fname, outname):
     n_joints = len(bodyparts)
     test = np.array(data).reshape(n_frames, n_joints, n_possible, 3)
 
-    points_full = test[:, :, :, :2]
-    scores_full = test[:, :, :, 2]
+    metadata = {
+        'bodyparts': bodyparts,
+        'scorer': scorer,
+        'index': data.index
+    }
+
+    return test, metadata
+
+def filter_pose_viterbi(config, all_points, bodyparts):
+    n_frames, n_joints, n_possible, _ = all_points.shape
+
+    points_full = all_points[:, :, :, :2]
+    scores_full = all_points[:, :, :, 2]
 
     points_full[scores_full < config['filter']['score_threshold']] = np.nan
 
@@ -308,11 +299,22 @@ def filter_pose_viterbi(config, fname, outname):
         points[:, jix] = pts_new
         scores[:, jix] = scs_new
 
+    return points, scores
+
+
+def write_pose_2d(all_points, metadata, outname):
+    points = all_points[:, :, 0, :2]
+    scores = all_points[:, :, 0, 2]
+
+    scorer = metadata['scorer']
+    bodyparts = metadata['bodyparts']
+    index = metadata['index']
+
     columns = pd.MultiIndex.from_product(
         [[scorer], bodyparts, ['x', 'y', 'likelihood']],
         names=['scorer', 'bodyparts', 'coords'])
 
-    dout = pd.DataFrame(columns=columns, index=data.index)
+    dout = pd.DataFrame(columns=columns, index=index)
 
     dout.loc[:, (scorer, bodyparts, 'x')] = points[:, :, 0]
     dout.loc[:, (scorer, bodyparts, 'y')] = points[:, :, 1]
@@ -321,24 +323,14 @@ def filter_pose_viterbi(config, fname, outname):
     dout.to_hdf(outname, 'df_with_missing', format='table', mode='w')
 
 
-def filter_pose_medfilt(config, fname, outname):
-    data_orig = pd.read_hdf(fname)
-    scorer = data_orig.columns.levels[0][0]
-    data = data_orig[scorer]
+def filter_pose_medfilt(config, all_points, bodyparts):
+    n_frames, n_joints, n_possible, _ = all_points.shape
 
-    bp_index = data.columns.names.index('bodyparts')
-    coord_index = data.columns.names.index('coords')
-    bodyparts = list(data.columns.get_level_values(bp_index).unique())
-    n_possible = len(data.columns.levels[coord_index])//3
+    points_full = all_points[:, :, :, :2]
+    scores_full = all_points[:, :, :, 2]
 
-    n_frames = len(data)
-    n_joints = len(bodyparts)
-    test = np.array(data).reshape(n_frames, n_joints, n_possible, 3)
-
-    points_full = test[:, :, :, :2]
-    scores_full = test[:, :, :, 2]
-
-    dout = data_orig.copy()
+    points = np.full((n_frames, n_joints, 2), np.nan, dtype='float64')
+    scores = np.empty((n_frames, n_joints), dtype='float64')
 
     for bp_ix, bp in enumerate(bodyparts):
         x = points_full[:, bp_ix, 0, 0]
@@ -373,20 +365,67 @@ def filter_pose_medfilt(config, fname, outname):
                     vals[nans] = np.interp(ix(nans), ix(~nans), vals[~nans])
             Xfi[:,i] = vals
 
-        dout[scorer, bp, 'x'] = Xfi[:, 0]
-        dout[scorer, bp, 'y'] = Xfi[:, 1]
-        dout[scorer, bp, 'interpolated'] = np.isnan(Xf[:, 0])
+        points[:, bp_ix, 0] = Xfi[:, 0]
+        points[:, bp_ix, 1] = Xfi[:, 1]
+        # dout[scorer, bp, 'interpolated'] = np.isnan(Xf[:, 0])
 
-    dout.to_hdf(outname, 'df_with_missing', format='table', mode='w')
+    scores = scores_full[:, :, 0]
 
+    return points, scores
+
+def filter_pose_autoencoder(config, all_points, bodyparts):
+    n_frames, n_joints, n_possible, _ = all_points.shape
+
+    points_full = all_points[:, :, :, :2]
+    scores_full = all_points[:, :, :, 2]
+
+    scores_test = all_points[:, :, 0, 2]
+
+    fname_model = config['filter']['autoencoder_path']
+    with open(fname_model, 'rb') as f:
+        mlp = pickle.load(f)
+
+    scores_pred = mlp.predict(scores_test)
+    scores_pred = np.clip(scores_pred, 0, 1)
+    scores_pred_rep = np.repeat(scores_pred, n_possible, axis=1).reshape(scores_full.shape)
+
+    scores_fixed = np.min([scores_pred_rep, scores_full], axis=0)
+
+    return points_full, scores_fixed
+
+def wrap_points(points, scores):
+    if len(points.shape) == 3: # n_possible = 1
+        points = points[:, :, None]
+        scores = scores[:, :, None]
+
+    n_frames, n_joints, n_possible, _ = points.shape
+
+    all_points = np.full((n_frames, n_joints, n_possible, 3), np.nan, dtype='float64')
+    all_points[:, :, :, :2] = points
+    all_points[:, :, :, 2] = scores
+
+    return all_points
+
+
+FILTER_MAPPING = {
+    'medfilt': filter_pose_medfilt,
+    'clusters': filter_pose_clusters,
+    'viterbi': filter_pose_viterbi,
+    'autoencoder': filter_pose_autoencoder
+}
+
+POSSIBLE_FILTERS = FILTER_MAPPING.keys()
 
 def process_session(config, session_path):
     pipeline_pose = config['pipeline']['pose_2d']
     pipeline_pose_filter = config['pipeline']['pose_2d_filter']
-    filter_type = config['filter']['type']
+    filter_types = config['filter']['type']
+    if not isinstance(filter_types, list):
+        filter_types = [filter_types]
 
-    assert filter_type in ['medfilt', 'clusters', 'viterbi'], \
-        "Invalid filter type, should be 'medfilt', 'viterbi', or 'clusters', but found {}".format(filter_type)
+    for filter_type in filter_types:
+        assert filter_type in POSSIBLE_FILTERS, \
+            "Invalid filter type, should be one of {}, but found {}".format(POSSIBLE_FILTERS, filter_type)
 
     pose_folder = os.path.join(session_path, pipeline_pose)
     output_folder = os.path.join(session_path, pipeline_pose_filter)
@@ -407,13 +446,14 @@ def process_session(config, session_path):
             continue
 
         print(outpath)
+        all_points, metadata = load_pose_2d(fname)
 
-        if config['filter']['type'] == 'medfilt':
-            filter_pose_medfilt(config, fname, outpath)
-        elif config['filter']['type'] == 'clusters':
-            filter_pose_clusters(config, fname, outpath)
-        elif config['filter']['type'] == 'viterbi':
-            filter_pose_viterbi(config, fname, outpath)
+        for filter_type in filter_types:
+            filter_fun = FILTER_MAPPING[filter_type]
+            points, scores = filter_fun(config, all_points, metadata['bodyparts'])
+            all_points = wrap_points(points, scores)
+
+        write_pose_2d(all_points, metadata, outpath)
 
 
 filter_pose_all = make_process_fun(process_session)
