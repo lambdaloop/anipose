7aed97167be65a9d5684aca4d1e41c7ab0929765
Pierre Karashchuk <krchtchk@gmail.com>
2020-01-02 17:04:23 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anipose",
-    version="0.6.3",
+    version="0.6.4",
     author="Pierre Karashchuk",
     author_email="krchtchk@gmail.com",
     description="Framework for scalable DeepLabCut based analysis including 3D tracking",
