e955bb17bafaf93f9cfe806fd879d18e0e8109c9
Pierre Karashchuk <krchtchk@gmail.com>
2020-04-01 16:43:54 -0700

    remove anipose.lib alias, bump version

--- anipose/__init__.py
+++ anipose/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import aniposelib as lib
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 VERSION = __version__
 
-sys.modules['anipose.lib'] = sys.modules['aniposelib']
