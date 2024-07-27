f314deee3b55ad23df4274f24fd38c12fd835a5d
Lili Karashchuk <krchtchk@gmail.com>
2024-07-27 12:34:56 -0700

    fix issue with bad caching of files for version

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rv dist
-python3 setup.py bdist_wheel
+rm -rv dist build *.egg-info
+python setup.py sdist bdist_wheel
+# python3 setup.py bdist_wheel
 twine upload dist/*
